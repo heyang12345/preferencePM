ed6922a0b11397648a79b8ba1d454df73890edde
heyang <2279598898@qq.com>
2017-06-02 11:59:09 +0800

    换路径测试

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,72 @@
 var gulp = require("gulp");
 //引入sass插件对应的模块
 var sass = require("gulp-sass");
+
 //引入合并文件对应的模块
 var concat = require("gulp-concat");
+
 //引入压缩的模块
 var uglify = require("gulp-uglify");
+
+//sass编译任务
+gulp.task("sass",function(){
+	gulp.src("sass/header.scss")//源文件
+	.pipe(sass())
+	.pipe(gulp.dest("dist/css"));//把结果放在何处。
+	
+});
+
+gulp.task("sass2",function(){
+	gulp.src("sass/main.scss")//源文件
+	.pipe(sass())
+	.pipe(gulp.dest("dist/css"));//把结果放在何处。
+	
+});
+gulp.task("sass3",function(){
+	gulp.src("sass/public.scss")//源文件
+	.pipe(sass())
+	.pipe(gulp.dest("dist/css"));//把结果放在何处。
+	
+});
+
+//合并
+gulp.task("concatJS",function(){
+	gulp.src(["js/todayUpdate.js","js/trademark.js"])
+	.pipe(concat("all.js"))
+	.pipe(gulp.dest("dist/js"));
+});
+
+//合并并压缩
+gulp.task("uglify",function(){
+	gulp.src(["js/todayUpdate.js","js/trademark.js"])
+	.pipe(concat("all.js"))//合并
+	.pipe(uglify())//压缩
+	.pipe(gulp.dest("dist/js"));
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
 //引入重命名的模块
 var rename = require("gulp-rename");
 //引入压缩CSS的模块
@@ -24,29 +86,10 @@ gulp.task("copy-images",function(){
 
 //
 
-//sass编译任务
-gulp.task("sass",function(){
-	gulp.src("common.scss")//源文件
-	.pipe(sass())//做sass预编译（做什么样的处理）
-	.pipe(gulp.dest("css/"));//把结果放在何处。
-	
-});
 
 
-//合并
-gulp.task("concatJS",function(){
-	gulp.src(["js/index.js","js/goodsList.js"])
-	.pipe(concat("all.js"))
-	.pipe(gulp.dest("dist/js"));
-});
 
-//合并并压缩
-gulp.task("uglify",function(){
-	gulp.src(["js/index.js","js/goodsList.js"])
-	.pipe(concat("all.js"))//合并
-	.pipe(uglify())//压缩
-	.pipe(gulp.dest("dist/js"));
-});
+
 
 //合并，压缩，重命名
 gulp.task("concatuglifyrename",function(){
@@ -57,7 +100,6 @@ gulp.task("concatuglifyrename",function(){
 	.pipe(rename("all.min.js"))
 	.pipe(gulp.dest("dist/js"));
 });
-
 
 gulp.task("minifycss",function(){
 	gulp.src("css/index.scss")
@@ -73,3 +115,4 @@ gulp.task("watchAll",function(){
 	
 });
 
+*/
