2d51ecb9730a4d460b7ca447c8b89da51d54333c
heyang <2279598898@qq.com>
2017-06-12 20:12:35 +0800

    初步完成

--- js/commodityDetails.js
+++ js/commodityDetails.js
@@ -18,21 +18,24 @@ $(function(){
 					for (var j=0;j<12;j++) {
 					
 						k=j%12;
+						
 						var divBox=document.createElement("div")
 							divBox.style.cssText="margin-bottom:30px;"
 							divBox.id="divBoxId"+obj[j].goodsId
 						var li=document.createElement("li");
 							li.style.cssText="width: 283px;"
 							li.ord=j;
-						var a=document.createElement("a");
+						var a=document.createElement("b");
 							a.style.cssText="position: relative;"
-							a.href="#"
+						
+	
+							//alert(a.id)
 						var img=document.createElement("img");
 							img.src="img/nv"+(k+1)+".jpg"
 							img.style.cssText="width: 283px;height: 283px;"
-						var aa=document.createElement("a")
+						var aa=document.createElement("b")
 							aa.style.cssText="font: 12px/18px '';color:#fff;"
-							aa.href="#"
+							
 							aa.innerHTML="找相似>"
 						var p=document.createElement("p");
 							p.style.cssText="width: 283px;height: 18px;background:rgba(147,147,147,.8);position: absolute;top: 131px;text-align: center;display: none;"
@@ -45,8 +48,8 @@ $(function(){
 							div.style.cssText="background: #fff;height: 62px;"
 						var H3=document.createElement("h3");
 							H3.style.cssText="font:12px/29px '';color: #666;margin-left: 18px;"
-						var a2=document.createElement("a");
-							a2.href="#"
+						var a2=document.createElement("b");
+							
 							a2.innerHTML=obj[j].goodsName
 							H3.appendChild(a2)
 							div.appendChild(H3)
@@ -69,8 +72,8 @@ $(function(){
 						var span=document.createElement("span");
 							span.style.cssText="font: 12px/25px '';color: #999;margin-left: 20px;"
 							span.innerHTML="已售"
-						var a3=document.createElement("a");
-							a3.href="#"
+						var a3=document.createElement("b");
+							
 							a3.innerHTML="4.5万件"
 							span.appendChild(a3)
 							div.appendChild(span)
@@ -100,7 +103,7 @@ $(function(){
 						var span5=document.createElement("span");
 							span5.style.cssText="color: #ff7e7c;font: 12px/28px '';"
 							
-							var a4=document.createElement("a");
+							var a4=document.createElement("b");
 								a4.href="#"
 								a4.style.cssText="color: #ff7e7c;font: 12px/28px '';"
 								a4.innerHTML="退货补贴优惠卷"
@@ -108,16 +111,16 @@ $(function(){
 							div2.appendChild(span5)	
 						var div3=document.createElement("div");
 							div3.style.cssText="float:right;margin-right: 5px;"
-						var a5=document.createElement("a");
-							a5.href="#"
+						var a5=document.createElement("b");
+							
 						var imgCollect=document.createElement("img");
 							imgCollect.src="img/xing1.png"
 							a5.appendChild(imgCollect)
 							a5.style.cssText="color: #aeaeae;font: 12px/28px '';margin-right: 5px;"	
 							a5.innerHTML="收藏"
 							div3.appendChild(a5)
-						var a6=document.createElement("a");	
-							a6.href="#"
+						var a6=document.createElement("b");	
+							
 							a6.style.cssText="color: #aeaeae;font: 12px/28px '';"
 							a6.innerHTML="[详情]"
 							div3.appendChild(a6)
@@ -130,13 +133,32 @@ $(function(){
 							todayUpDateJS.appendChild(ul)	
 					li.onmouseover=showImg;
 					li.onmouseout=hideImg;
+					li.onclick=skip;
 					
 				}
 				}
+				
+				function skip(){
+					var ord=this.ord+1;
+					
+					location.href="particulars.html";
+					saveCookie("goodsId",ord,7)	
+				}
+				//存储商品Id
+				function saveCookie(key,value,dayCount){
+						var d = new Date();
+						d.setDate(d.getDate()+dayCount);
+						document.cookie = key+"="+encodeURIComponent
+					
+					(value)+";expires="+d.toGMTString();
+					}
+				//显示
 				function showImg(){
+					
 					this.style.cssText="box-shadow: 0px 4px 20px #999;"
 					//alert(this.ord)
 					var ord=this.ord;
+					
 					var p = document.getElementById("pId"+ord)
 					p.style.cssText="width: 283px;height: 18px;background:rgba(147,147,147,.8);position: absolute;top: 131px;text-align: center;display: block;"
 					
@@ -148,6 +170,7 @@ $(function(){
 					divId.style.cssText="height: 28px;border-top:1px dashed #ececec;background: #fff;display: block;"
 					
 				}
+				//隐藏
 				function hideImg(){
 					this.style.cssText="box-shadow: 0px 0px 0px #999;"
 					var ord=this.ord;
